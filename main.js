663e088c5dc7ff3a4df1797424ea0530f6e155df
Nenoplayhard52 <125273548+Nenoplayhards2@users.noreply.github.com>
2023-09-26 14:34:10 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -86,30 +86,3 @@ function segundosParaMinutos(segundos){
 }
 
 
-// função da senha
-function mostrarsenha(){
-   var inputPass = document.getElementById('senha')
-   var btnShowPass = document.getElementById('Btn-senha')
-
-   if(inputPass.type === 'password'){
-   inputPass.setAttribute('type','text')
-   btnShowPass.classList.replace('bi-eye','bi-eye-slash')
-}
-else{
-   inputPass.setAttribute('type','password')
-   btnShowPass.classList.replace('bi-eye-slash','bi-eye')
-}
-}
-function mostrarSenha(){
-   var inputPass = document.getElementById('Crie')
-   var btnShowPass = document.getElementById('Btn-Senha')
-
-   if(inputPass.type === 'password'){
-   inputPass.setAttribute('type','text')
-   btnShowPass.classList.replace('bi-eye','bi-eye-slash')
-}
-else{
-   inputPass.setAttribute('type','password')
-   btnShowPass.classList.replace('bi-eye-slash','bi-eye')
-}
-}
